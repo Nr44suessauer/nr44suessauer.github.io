ee3fe06dd0a91bf81dbe53631ad0607eadecc882
Nr44suessauer <56438338+Nr44suessauer@users.noreply.github.com>
2024-12-26 11:08:38 +0000

    Deploying to gh-pages from @ Nr44suessauer/nr44suessauer.github.io@c40a2a475dbe94f1722b26e4a01cd3dbefa9449c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,35 +492,42 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%4D%61%72%63.%4E%61%75%65%6E%64%6F%72%66@%48%73-%48%65%69%6C%62%72%6F%6E%6E.%64%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Nr44suessauer", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-gitlab',
+        title: 'GitLab',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://gitlab.com/Nauendorf", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://instagram.com/nr44mitreis", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/marc-nauendorf-266234329", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.Instagramm.com/nr44mitreis", "_blank");
         },
       },{
       id: 'light-theme',
