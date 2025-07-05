a433ec09301a5fecf00b2770cc3fe35653abec4a
Marc <marcnauendorfbusiness@gmail.com>
2025-07-05 11:01:46 +0200

    Deploy website - 2025-07-05 11:01:44

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -216,6 +216,7 @@ export default defineNuxtConfig({
 
   nitro: {
     prerender: {
+      routes: ['/elopment'],
       ignore: ['/__pinceau_tokens_config.json', '/__pinceau_tokens_schema.json']
     },
     compressPublicAssets: true
