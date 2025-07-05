c141fdf965c88fdce1800fa0c5da0373d057c10c
Marc <marcnauendorfbusiness@gmail.com>
2025-07-05 10:55:10 +0200

    Deploy website - 2025-07-05 10:55:08

---
+++ pages/elopment.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div style="display: flex; justify-content: center; align-items: center; min-height: 50vh;">
+      <a href="https://phdcomics.com/comics/archive_list.php" target="_blank">
+        <img src="http://www.phdcomics.com/comics/archive/phd072915s.gif" alt="PhD Comics" style="max-width: 100%; height: auto;" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// Page meta
+definePageMeta({
+  title: 'elopment',
+  description: 'Development insights and methodologies',
+  layout: 'default'
+})
+
+// SEO
+useHead({
+  title: 'elopment',
+  meta: [
+    { name: 'description', content: 'Development insights and methodologies' },
+    { property: 'og:image', content: '/social-card-preview.png' }
+  ]
+})
+</script>
